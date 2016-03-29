bd6bfa6a7af2e84e68c3417dde2a915a8b5b2de7
Rijk van Zanten <rijkvanzanten@me.com>
2016-03-29 12:34:43 +0200

    In void

--- ultrasone_sensor-zonder-lampjes.ino
+++ ultrasone_sensor-zonder-lampjes.ino
@@ -8,23 +8,22 @@ void setup() {
   Serial.begin(9600);
 }
 
-void loop() {
-  delay(60);
-
-  // http://www.instructables.com/id/Simple-Arduino-and-HC-SR04-Example/
-
+void distance() {
   long duration, distance;
-  
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-  
   duration = pulseIn(echoPin, 75);
-  
   distance = (duration / 2) / 29.1;
+  return distance;
+}
 
+void loop() {
+  long distance = distance();
+  
   Serial.println(distance);
+  delay(60);
 }
 
